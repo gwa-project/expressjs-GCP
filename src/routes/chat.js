e21dcd40449b617d093613ac86729c01584f0b25
“GilarYa” <gilarwahibul@gmail.com>
2025-10-06 00:14:15 +0700

    update chats

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { chat } from '../controllers/chatController.js';
-import asyncHandler from '../middleware/asyncHandler.js';
+import { asyncHandler } from '../middleware/asyncHandler.js';
 
 const router = express.Router();
 
